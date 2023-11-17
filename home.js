8ab8a8c74527468c0ee86911997386860faa980a
am4z0n <ainainioluwa090@gmail.com>
2023-11-17 05:56:09 -0800

    initial update

---
+++ home.js
@@ -0,0 +1,12 @@
+console.log('hello')
+
+const look = (mee) =>   console.log('its you')
+look();
+
+/*let numbers = [10, 20, 30, 40] => {
+    console.log(length);
+}
+numbers();
+*/
+
+alert(console.log('weldone'))
